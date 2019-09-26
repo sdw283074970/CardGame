ac20ef2b1d1c782e281b1b382922bbdb5ca4ad5c
sdw283074970 <downwes@gmail.com>
2019-09-25 18:09:40 -0700

    增加了编辑器键菜单选项，用于快速实例化scriptable object。

---
+++ Assets/Scripts/SO/Editor/CardUnityIntergration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CardUnityIntergration : MonoBehaviour
+{
+    [MenuItem("Assets/Create/Card Assets/Negotiation Card")]
+    public static void CreateNegotiationCardScriptableObject()
+    {
+        ScriptableObjectUtility.CreateAsset<NegotiationCard>();
+    }
+}
+
+public static class ScriptableObjectUtility
+{
+    public static void  CreateAsset<T>() where T : ScriptableObject
+    {
+        var asset = ScriptableObject.CreateInstance<T>();
+        ProjectWindowUtil.CreateAsset(asset, "New " + typeof(T).Name + ".asset");
+    }
+}
+
