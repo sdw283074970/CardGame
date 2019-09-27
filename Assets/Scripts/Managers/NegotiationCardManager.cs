1812caf63a828742276fb76b01afa21c7fe71440
sdw283074970 <downwes@gmail.com>
2019-09-26 17:22:47 -0700

    开始制作谈判卡manager

---
+++ Assets/Scripts/Managers/NegotiationCardManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NegotiationCardManager : MonoBehaviour
+{
+    public NegotiationCard NegotiationCardAsset;
+
+    [Header("Text Ref")]
+    public string NameText;
+    public string DescriptionText;
+    public int Power;
+    public int Times;
+    public int Cost;
+
+    [Header("Image Frame Ref")]
+    public Sprite CardFaceFrameImage;
+    public Sprite CardFaceGlowImage;
+    public Sprite CardBackGlowImage;
+    public Sprite CardBodyImage;
+
+    [Header("Image Ctor Ref")]
+    public Sprite CardCostImage;
+    public Sprite CardTitleImage;
+    public Sprite RarityStoneImage;
+    public Sprite CardGraphicImage;
+    public Sprite CardDescriptionImage;
+    public Sprite CardPowerImage; //左下角
+    public Sprite CardTimesImage; //右下角
+}
--- Assets/Scripts/SO/ScriptableCards.cs
+++ Assets/Scripts/SO/ScriptableCards.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Assets.Scripts;
 
 public enum TargetingOptions
 {
@@ -35,14 +36,18 @@ public class NegotiationCard : ScriptableObject, IComparable<NegotiationCard>
 {
     [Header("General Info")]
     public string CardName;
+    [PreviewSprite]
     public Sprite CardTemplate;
+    [PreviewSprite]
     public Sprite CardImage;
-    [Range(1, 1000)]
+    [Range(0, 1000)]
     public int Cost;
     [TextArea(2, 3)]
     public string Description;
     [TextArea(2, 3)]
     public string Tags;
+    [Range(0, 2)]
+    public int RarityLevel;
     public TypeOfCard TypeOfCard;
     public TypeOfNegotiationCard TypeOfNegotiationCard;
     public RarityOptions Rarity;
@@ -50,11 +55,11 @@ public class NegotiationCard : ScriptableObject, IComparable<NegotiationCard>
     [Header("Negotiation Info")]
     [Range(0, 2)]
     public float Times;
-    [Range(1, 1000)]
-    public int ThreatValue;
-    [Range(1, 1000)]
+    [Range(0, 1000)]
+    public int DeterrenceValue;
+    [Range(0, 1000)]
     public int CounterValue;
-    [Range(1, 1000)]
+    [Range(0, 1000)]
     public int BargainingChip;
 
     [Header("Strategy Info")]
